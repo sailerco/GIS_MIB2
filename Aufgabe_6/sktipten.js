e4f177ff793c1c35e999541c6335d54d265eac41
corinna.sailer <corinna.jasmin.sailer@hs-furtwangen.de>
2020-06-03 09:29:49 +0200

    ts update

--- Aufgabe_6/sktipten.js
+++ Aufgabe_6/sktipten.js
@@ -1,174 +1,30 @@
 "use strict";
 var Aufgabe_6;
 (function (Aufgabe_6) {
-    let eevee = {
-        kategorie: true,
-        name: "Evoli Rucksack",
-        image: "poke/bag.jpg",
-        description: "Rucksack - Evoli Design",
-        price: 20
-    };
-    let relaxo = {
-        kategorie: true,
-        name: "Relaxo Sitzsack",
-        image: "poke/beanbag.jpg",
-        description: "1,20m, weich, Relaxo",
-        price: 100
-    };
-    let a3 = {
-        kategorie: true,
-        name: "Detektiv Pikachu",
-        image: "poke/cap.jpg",
-        description: "Mütze mit Pikachu Ohren",
-        price: 15
-    };
-    let a4 = {
-        kategorie: true,
-        name: "Zurrokex Mütze",
-        image: "poke/cap2.jpg",
-        description: "Mütze im Zurrokex Design",
-        price: 15
-    };
-    let a5 = {
-        kategorie: true,
-        name: "Evoli Figuren",
-        image: "poke/eevee.jpg",
-        description: "Evolis sind abnehmbar",
-        price: 20
-    };
-    let a6 = {
-        kategorie: true,
-        name: "Gengar Tasse",
-        image: "poke/mug.jpg",
-        description: "Lila Tasse mit Gengars Gesicht",
-        price: 7
-    };
-    let a7 = {
-        kategorie: true,
-        name: "Relaxo Plüschtier",
-        image: "poke/plush1.jpg",
-        description: "Sehr weiches Relaxo",
-        price: 15
-    };
-    let a8 = {
-        kategorie: true,
-        name: "Wolly Plüschtier",
-        image: "poke/plush2.jpg",
-        price: 15,
-        description: "Sehr weiches Wolly"
-    };
-    let a9 = {
-        kategorie: true,
-        name: "Pikachu Plüschtier",
-        image: "poke/plush3.jpg",
-        price: 20,
-        description: "Pikachu macht sein Abschluss"
-    };
-    let a10 = {
-        kategorie: true,
-        name: "Ditto Plüschtier",
-        image: "poke/plush4.jpg",
-        price: 15,
-        description: "Passt zu jeder Einrichtung"
-    };
-    let a11 = {
-        kategorie: true,
-        name: "Poké-Ball Schüssel",
-        image: "poke/pokego.png",
-        price: 15,
-        description: "Spülmaschinengeeignet"
-    };
-    let a12 = {
-        kategorie: true,
-        name: "Poster",
-        image: "poke/poster.jpg",
-        price: 15,
-        description: "Gen 1 Poster"
-    };
-    let sw1 = {
-        kategorie: false,
-        name: "Yoda Rucksack",
-        image: "sw/babybag.jpg",
-        price: 30,
-        description: "Kinderrucksack"
-    };
-    let sw2 = {
-        kategorie: false,
-        name: "Baby Yoda",
-        image: "sw/babyyoda.jpg",
-        price: 40,
-        description: "Perfekte Einrichtung"
-    };
-    let sw3 = {
-        kategorie: false,
-        name: "BB8 Brotdose",
-        image: "sw/brotdose-bb-8.jpg",
-        price: 15,
-        description: "Luftdicht"
-    };
-    let sw4 = {
-        kategorie: false,
-        name: "Ewok Rucksack",
-        image: "sw/ewokbag.jpg",
-        price: 30,
-        description: "Sehr flauschig"
-    };
-    let sw5 = {
-        kategorie: false,
-        name: "Lichtschwert",
-        image: "sw/lightsaber.jpg",
-        price: 150,
-        description: "RGB!"
-    };
-    let sw6 = {
-        kategorie: false,
-        name: "Tasse D-O",
-        image: "sw/mug.jpg",
-        price: 7,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw7 = {
-        kategorie: false,
-        name: "Tasse BB8",
-        image: "sw/mug2.jpg",
-        price: 7,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw8 = {
-        kategorie: false,
-        name: "Tasse Porg",
-        image: "sw/porg.jpg",
-        price: 7,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw9 = {
-        kategorie: false,
-        name: "R2D2 Kaffekanne",
-        image: "sw/coffee.jpg",
-        price: 30,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw10 = {
-        kategorie: false,
-        name: "R2D2 Reiskocher",
-        image: "sw/Pot.jpg",
-        price: 90,
-        description: "Ant-Haft-Beschichtet"
-    };
-    let sw11 = {
-        kategorie: false,
-        name: "Darth Vader Toaster",
-        image: "sw/toaster.jpg",
-        price: 30,
-        description: "Perfekt für jedes Frühstück"
-    };
-    let sw12 = {
-        kategorie: false,
-        name: "Star Wars Waffeleisen",
-        image: "sw/waffels.jpg",
-        price: 30,
-        description: "Todesstern Waffeln!"
-    };
+    let eevee = { kategorie: true, name: "Evoli Rucksack", image: "poke/bag.jpg", description: "Rucksack - Evoli Design", price: 20 };
+    let relaxo = { kategorie: true, name: "Relaxo Sitzsack", image: "poke/beanbag.jpg", description: "1,20m, weich, Relaxo", price: 100 };
+    let a3 = { kategorie: true, name: "Detektiv Pikachu", image: "poke/cap.jpg", description: "Mütze mit Pikachu Ohren", price: 15 };
+    let a4 = { kategorie: true, name: "Zurrokex Mütze", image: "poke/cap2.jpg", description: "Mütze im Zurrokex Design", price: 15 };
+    let a5 = { kategorie: true, name: "Evoli Figuren", image: "poke/eevee.jpg", description: "Evolis sind abnehmbar", price: 20 };
+    let a6 = { kategorie: true, name: "Gengar Tasse", image: "poke/mug.jpg", description: "Lila Tasse mit Gengars Gesicht", price: 7 };
+    let a7 = { kategorie: true, name: "Relaxo Plüschtier", image: "poke/plush1.jpg", description: "Sehr weiches Relaxo", price: 15 };
+    let a8 = { kategorie: true, name: "Wolly Plüschtier", image: "poke/plush2.jpg", description: "Sehr weiches Wolly", price: 15 };
+    let a9 = { kategorie: true, name: "Pikachu Plüschtier", image: "poke/plush3.jpg", description: "Pikachu macht sein Abschluss", price: 20 };
+    let a10 = { kategorie: true, name: "Ditto Plüschtier", image: "poke/plush4.jpg", description: "Passt zu jeder Einrichtung", price: 15 };
+    let a11 = { kategorie: true, name: "Poké-Ball Schüssel", image: "poke/pokego.png", description: "Spülmaschinengeeignet", price: 15 };
+    let a12 = { kategorie: true, name: "Poster", image: "poke/poster.jpg", description: "Gen 1 Poster", price: 15 };
+    let sw1 = { kategorie: false, name: "Yoda Rucksack", image: "sw/babybag.jpg", description: "Kinderrucksack", price: 30 };
+    let sw2 = { kategorie: false, name: "Baby Yoda", image: "sw/babyyoda.jpg", description: "Perfekte Einrichtung", price: 40 };
+    let sw3 = { kategorie: false, name: "BB8 Brotdose", image: "sw/brotdose-bb-8.jpg", description: "Luftdicht", price: 15 };
+    let sw4 = { kategorie: false, name: "Ewok Rucksack", image: "sw/ewokbag.jpg", description: "Sehr flauschig", price: 30 };
+    let sw5 = { kategorie: false, name: "Lichtschwert", image: "sw/lightsaber.jpg", description: "RGB!", price: 150 };
+    let sw6 = { kategorie: false, name: "Tasse D-O", image: "sw/mug.jpg", description: "Spülmaschinengeeignet", price: 7 };
+    let sw7 = { kategorie: false, name: "Tasse BB8", image: "sw/mug2.jpg", description: "Spülmaschinengeeignet", price: 7 };
+    let sw8 = { kategorie: false, name: "Tasse Porg", image: "sw/porg.jpg", description: "Spülmaschinengeeignet", price: 7 };
+    let sw9 = { kategorie: false, name: "R2D2 Kaffekanne", image: "sw/coffee.jpg", description: "Spülmaschinengeeignet", price: 30 };
+    let sw10 = { kategorie: false, name: "R2D2 Reiskocher", image: "sw/Pot.jpg", description: "Ant-Haft-Beschichtet", price: 90 };
+    let sw11 = { kategorie: false, name: "Darth Vader Toaster", image: "sw/toaster.jpg", description: "Perfekt für jedes Frühstück", price: 30 };
+    let sw12 = { kategorie: false, name: "Star Wars Waffeleisen", image: "sw/waffels.jpg", description: "Todesstern Waffeln!", price: 30 };
     //#enderegion
     const pokemonartikel = [eevee, relaxo, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, sw11, sw12];
     const poke = document.getElementById("shop1");
--- Aufgabe_6/sktipten.ts
+++ Aufgabe_6/sktipten.ts
@@ -7,176 +7,32 @@ namespace Aufgabe_6 {
         description: string;
         price: number;
     }
-    let eevee: Pokestar = {
-        kategorie: true,
-        name: "Evoli Rucksack",
-        image: "poke/bag.jpg",
-        description: "Rucksack - Evoli Design",
-        price: 20
-    };
-    let relaxo: Pokestar = {
-        kategorie: true,
-        name: "Relaxo Sitzsack",
-        image: "poke/beanbag.jpg",
-        description: "1,20m, weich, Relaxo",
-        price: 100
-    };
-    let a3: Pokestar = {
-        kategorie: true,
-        name: "Detektiv Pikachu",
-        image: "poke/cap.jpg",
-        description: "Mütze mit Pikachu Ohren",
-        price: 15
-    };
-    let a4: Pokestar = {
-        kategorie: true,
-        name: "Zurrokex Mütze",
-        image: "poke/cap2.jpg",
-        description: "Mütze im Zurrokex Design",
-        price: 15
-    };
-    let a5: Pokestar = {
-        kategorie: true,
-        name: "Evoli Figuren",
-        image: "poke/eevee.jpg",
-        description: "Evolis sind abnehmbar",
-        price: 20
-    };
-    let a6: Pokestar = {
-        kategorie: true,
-        name: "Gengar Tasse",
-        image: "poke/mug.jpg",
-        description: "Lila Tasse mit Gengars Gesicht",
-        price: 7
-    };
-    let a7: Pokestar = {
-        kategorie: true,
-        name: "Relaxo Plüschtier",
-        image: "poke/plush1.jpg",
-        description: "Sehr weiches Relaxo",
-        price: 15
-    };
-    let a8: Pokestar = {
-        kategorie: true,
-        name: "Wolly Plüschtier",
-        image: "poke/plush2.jpg",
-        price: 15,
-        description: "Sehr weiches Wolly"
-    };
-    let a9: Pokestar = {
-        kategorie: true,
-        name: "Pikachu Plüschtier",
-        image: "poke/plush3.jpg",
-        price: 20,
-        description: "Pikachu macht sein Abschluss"
-    };
-    let a10: Pokestar = {
-        kategorie: true,
-        name: "Ditto Plüschtier",
-        image: "poke/plush4.jpg",
-        price: 15,
-        description: "Passt zu jeder Einrichtung"
-    };
-    let a11: Pokestar = {
-        kategorie: true,
-        name: "Poké-Ball Schüssel",
-        image: "poke/pokego.png",
-        price: 15,
-        description: "Spülmaschinengeeignet"
-    };
-    let a12: Pokestar = {
-        kategorie: true,
-        name: "Poster",
-        image: "poke/poster.jpg",
-        price: 15,
-        description: "Gen 1 Poster"
-    };
-    let sw1: Pokestar = {
-        kategorie: false,
-        name: "Yoda Rucksack",
-        image: "sw/babybag.jpg",
-        price: 30,
-        description: "Kinderrucksack"
-    };
-    let sw2: Pokestar = {
-        kategorie: false,
-        name: "Baby Yoda",
-        image: "sw/babyyoda.jpg",
-        price: 40,
-        description: "Perfekte Einrichtung"
-    };
-    let sw3: Pokestar = {
-        kategorie: false,
-        name: "BB8 Brotdose",
-        image: "sw/brotdose-bb-8.jpg",
-        price: 15,
-        description: "Luftdicht"
-    };
-    let sw4: Pokestar = {
-        kategorie: false,
-        name: "Ewok Rucksack",
-        image: "sw/ewokbag.jpg",
-        price: 30,
-        description: "Sehr flauschig"
-    };
-    let sw5: Pokestar = {
-        kategorie: false,
-        name: "Lichtschwert",
-        image: "sw/lightsaber.jpg",
-        price: 150,
-        description: "RGB!"
-    };
-
-    let sw6: Pokestar = {
-        kategorie: false,
-        name: "Tasse D-O",
-        image: "sw/mug.jpg",
-        price: 7,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw7: Pokestar = {
-        kategorie: false,
-        name: "Tasse BB8",
-        image: "sw/mug2.jpg",
-        price: 7,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw8: Pokestar = {
-        kategorie: false,
-        name: "Tasse Porg",
-        image: "sw/porg.jpg",
-        price: 7,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw9: Pokestar = {
-        kategorie: false,
-        name: "R2D2 Kaffekanne",
-        image: "sw/coffee.jpg",
-        price: 30,
-        description: "Spülmaschinengeeignet"
-    };
-    let sw10: Pokestar = {
-        kategorie: false,
-        name: "R2D2 Reiskocher",
-        image: "sw/Pot.jpg",
-        price: 90,
-        description: "Ant-Haft-Beschichtet"
-    };
-    let sw11: Pokestar = {
-        kategorie: false,
-        name: "Darth Vader Toaster",
-        image: "sw/toaster.jpg",
-        price: 30,
-        description: "Perfekt für jedes Frühstück"
-    };
-    let sw12: Pokestar = {
-        kategorie: false,
-        name: "Star Wars Waffeleisen",
-        image: "sw/waffels.jpg",
-        price: 30,
-        description: "Todesstern Waffeln!"
-    };
+    let eevee: Pokestar = { kategorie: true, name: "Evoli Rucksack", image: "poke/bag.jpg", description: "Rucksack - Evoli Design", price: 20 };
+    let relaxo: Pokestar = { kategorie: true, name: "Relaxo Sitzsack", image: "poke/beanbag.jpg", description: "1,20m, weich, Relaxo", price: 100 };
+    let a3: Pokestar = { kategorie: true, name: "Detektiv Pikachu", image: "poke/cap.jpg", description: "Mütze mit Pikachu Ohren", price: 15 };
+    let a4: Pokestar = {kategorie: true, name: "Zurrokex Mütze", image: "poke/cap2.jpg", description: "Mütze im Zurrokex Design",price: 15 };
+    let a5: Pokestar = { kategorie: true, name: "Evoli Figuren", image: "poke/eevee.jpg", description: "Evolis sind abnehmbar", price: 20};
+    let a6: Pokestar = { kategorie: true, name: "Gengar Tasse", image: "poke/mug.jpg", description: "Lila Tasse mit Gengars Gesicht", price: 7};
+    let a7: Pokestar = { kategorie: true, name: "Relaxo Plüschtier", image: "poke/plush1.jpg", description: "Sehr weiches Relaxo", price: 15};
+    let a8: Pokestar = { kategorie: true, name: "Wolly Plüschtier", image: "poke/plush2.jpg", description: "Sehr weiches Wolly", price: 15};
+    let a9: Pokestar = { kategorie: true, name: "Pikachu Plüschtier", image: "poke/plush3.jpg", description: "Pikachu macht sein Abschluss", price: 20};
+    let a10: Pokestar = { kategorie: true, name: "Ditto Plüschtier", image: "poke/plush4.jpg", description: "Passt zu jeder Einrichtung", price: 15};
+    let a11: Pokestar = { kategorie: true, name: "Poké-Ball Schüssel", image: "poke/pokego.png", description: "Spülmaschinengeeignet", price: 15};
+    let a12: Pokestar = { kategorie: true, name: "Poster", image: "poke/poster.jpg", description: "Gen 1 Poster", price: 15};
+    let sw1: Pokestar = { kategorie: false, name: "Yoda Rucksack", image: "sw/babybag.jpg", description: "Kinderrucksack", price: 30};
+    let sw2: Pokestar = { kategorie: false, name: "Baby Yoda", image: "sw/babyyoda.jpg", description: "Perfekte Einrichtung", price: 40};
+    let sw3: Pokestar = { kategorie: false, name: "BB8 Brotdose", image: "sw/brotdose-bb-8.jpg", description: "Luftdicht", price: 15};
+    let sw4: Pokestar = { kategorie: false, name: "Ewok Rucksack", image: "sw/ewokbag.jpg", description: "Sehr flauschig", price: 30};
+    let sw5: Pokestar = { kategorie: false, name: "Lichtschwert", image: "sw/lightsaber.jpg", description: "RGB!", price: 150};
+    let sw6: Pokestar = { kategorie: false, name: "Tasse D-O", image: "sw/mug.jpg", description: "Spülmaschinengeeignet", price: 7};
+    let sw7: Pokestar = { kategorie: false, name: "Tasse BB8", image: "sw/mug2.jpg", description: "Spülmaschinengeeignet", price: 7};
+    let sw8: Pokestar = { kategorie: false, name: "Tasse Porg", image: "sw/porg.jpg", description: "Spülmaschinengeeignet", price: 7};
+    let sw9: Pokestar = { kategorie: false, name: "R2D2 Kaffekanne", image: "sw/coffee.jpg", description: "Spülmaschinengeeignet", price: 30};
+    let sw10: Pokestar = { kategorie: false, name: "R2D2 Reiskocher", image: "sw/Pot.jpg", description: "Ant-Haft-Beschichtet", price: 90 };
+    let sw11: Pokestar = { kategorie: false, name: "Darth Vader Toaster", image: "sw/toaster.jpg", description: "Perfekt für jedes Frühstück", price: 30 };
+    let sw12: Pokestar = { kategorie: false, name: "Star Wars Waffeleisen", image: "sw/waffels.jpg", description: "Todesstern Waffeln!", price: 30};
     //#enderegion
+    
     const pokemonartikel: Pokestar[] = [eevee, relaxo, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, sw11, sw12];
     const poke: HTMLElement = document.getElementById("shop1") as HTMLDivElement;
     const swtag: HTMLElement = document.getElementById("shop2") as HTMLDivElement;
