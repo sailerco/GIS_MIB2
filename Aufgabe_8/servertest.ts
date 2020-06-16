33144710d267383312fb06586b5bbf08523c2d96
corinna.sailer <corinna.jasmin.sailer@hs-furtwangen.de>
2020-06-16 19:16:27 +0200

    server

--- Aufgabe_8/servertest.ts
+++ Aufgabe_8/servertest.ts
@@ -1,55 +1,34 @@
-//import * as Http from "http";
+import * as Http from "http";
 
 namespace Aufgabe_8 {
- console.log("Starting server");
- /* let port: number = Number(process.env.PORT);
- if (!port)
-    port = 8100;  */
- let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
- button.addEventListener("click", buttonclick);
+  console.log("Starting server");
+  let port: number = Number(process.env.PORT);
+  if (!port)
+    port = 8100;
 
- async function urlbekommen(): Promise<string> {
-      let formData: FormData = new FormData(document.forms[0]);
-      let url: string = "https://dedflake.herokuapp.com/";
-      let query: URLSearchParams = new URLSearchParams(<any>formData);
-      url += url + "?" + query.toString();
-      return url;
-    }
+  let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+  button.addEventListener("click", buttonclick);
 
- async function buttonclick(): Promise<void> {
-    Response(await urlbekommen());
+  function buttonclick(): void {
+    
   }
- /* let server: Http.Server = Http.createServer(); */
- /* server.addListener("request", handleRequest);
- server.addListener("listening", handleListen);
- server.listen(port);  */
+  let server: Http.Server = Http.createServer();
+  server.addListener("request", handleRequest);
+  server.addListener("listening", handleListen);
+  server.listen(port);
   
- /* async function communicate(): Promise<string> {
-    let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "https://dedflake.herokuapp.com/";
-    let query: URLSearchParams = new URLSearchParams(<any>formData);
-    url += url + "?" + query.toString();
-    await fetch(url);
-    return url;
-  } */
+  function handleListen(): void {
+    console.log("Listening");
+  }
 
-/*  async function response(_url: RequestInfo): Promise<void> { //asynchron damit await fetch benutzt werden kann 
-    let response: Response = await fetch(_url);
-    let response2: string = await response.json();
-    console.log(response2);
-} */
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("I hear voices!");
 
- 
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
 
- async function Response(_url: RequestInfo /*_response: Http.ServerResponse */): Promise<void> {
-  let response: Response = await fetch(_url, { method:  "get" });
-  let response2: string = await response.text();
-  console.log(response2);
-  /* _response.setHeader("content-type", "text/html; charset=utf-8");
-  _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.write(_request.url);
 
-  _response.write(_url);
-
-  _response.end();  */
-  } 
+    _response.end();
+  }
 }
