4b137613640d949a537d89831f3baeec2303090a
corinna.sailer <corinna.jasmin.sailer@hs-furtwangen.de>
2020-06-16 19:05:07 +0200

    server

--- Aufgabe_8/servertest.js
+++ Aufgabe_8/servertest.js
@@ -1,21 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Http = require("http");
 var Aufgabe_8;
 (function (Aufgabe_8) {
-    /*  console.log("Starting server");
-     let port: number = Number(process.env.PORT);
-     if (!port)
-       port = 8100; */
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
     let button = document.getElementById("button");
     button.addEventListener("click", buttonclick);
     async function buttonclick() {
-        response(await communicate());
+        handleRequest(await handleListen());
     }
-    /* let server: Http.Server = Http.createServer();
+    let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
-    server.listen(port); */
-    async function communicate() {
+    server.listen(port);
+    /* async function communicate(): Promise<string> {
+       let formData: FormData = new FormData(document.forms[0]);
+       let url: string = "https://dedflake.herokuapp.com/";
+       let query: URLSearchParams = new URLSearchParams(<any>formData);
+       url += url + "?" + query.toString();
+       await fetch(url);
+       return url;
+     } */
+    /*  async function response(_url: RequestInfo): Promise<void> { //asynchron damit await fetch benutzt werden kann
+        let response: Response = await fetch(_url);
+        let response2: string = await response.json();
+        console.log(response2);
+    } */
+    async function handleListen() {
         let formData = new FormData(document.forms[0]);
         let url = "https://dedflake.herokuapp.com/";
         let query = new URLSearchParams(formData);
@@ -23,24 +37,16 @@ var Aufgabe_8;
         await fetch(url);
         return url;
     }
-    async function response(_url) {
+    async function handleRequest(_url /*_response: Http.ServerResponse */) {
         let response = await fetch(_url);
         let response2 = await response.json();
         console.log(response2);
+        /* _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+      
+        _response.write(_url);
+      
+        _response.end();  */
     }
-    /* function handleListen(): void {
-      console.log("Listening");
-    }
-  
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-      console.log("I hear voices!");
-  
-      _response.setHeader("content-type", "text/html; charset=utf-8");
-      _response.setHeader("Access-Control-Allow-Origin", "*");
-  
-      _response.write(_request.url);
-  
-      _response.end();
-    } */
 })(Aufgabe_8 || (Aufgabe_8 = {}));
 //# sourceMappingURL=servertest.js.map
--- Aufgabe_8/servertest.ts
+++ Aufgabe_8/servertest.ts
@@ -1,22 +1,37 @@
 import * as Http from "http";
 
 namespace Aufgabe_8 {
- /*  console.log("Starting server");
-  let port: number = Number(process.env.PORT);
-  if (!port)
-    port = 8100; */
-  let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-  button.addEventListener("click", buttonclick);
+ console.log("Starting server");
+ let port: number = Number(process.env.PORT);
+ if (!port)
+    port = 8100; 
+ let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+ button.addEventListener("click", buttonclick);
 
-  async function buttonclick(): Promise<void> {
-    response(await communicate());
+ async function buttonclick(): Promise<void> {
+    handleRequest(await handleListen());
   }
-  /* let server: Http.Server = Http.createServer();
-  server.addListener("request", handleRequest);
-  server.addListener("listening", handleListen);
-  server.listen(port); */
+ let server: Http.Server = Http.createServer();
+ server.addListener("request", handleRequest);
+ server.addListener("listening", handleListen);
+ server.listen(port); 
   
-  async function communicate(): Promise<string> {
+ /* async function communicate(): Promise<string> {
+    let formData: FormData = new FormData(document.forms[0]);
+    let url: string = "https://dedflake.herokuapp.com/";
+    let query: URLSearchParams = new URLSearchParams(<any>formData);
+    url += url + "?" + query.toString();
+    await fetch(url);
+    return url;
+  } */
+
+/*  async function response(_url: RequestInfo): Promise<void> { //asynchron damit await fetch benutzt werden kann 
+    let response: Response = await fetch(_url);
+    let response2: string = await response.json();
+    console.log(response2);
+} */
+
+ async function handleListen(): Promise<string> {
     let formData: FormData = new FormData(document.forms[0]);
     let url: string = "https://dedflake.herokuapp.com/";
     let query: URLSearchParams = new URLSearchParams(<any>formData);
@@ -25,24 +40,15 @@ namespace Aufgabe_8 {
     return url;
   }
 
-  async function response(_url: RequestInfo): Promise<void> { //asynchron damit await fetch benutzt werden kann 
-    let response: Response = await fetch(_url);
-    let response2:string = await response.json();
-    console.log(response2);
-}
+ async function handleRequest(_url: RequestInfo /*_response: Http.ServerResponse */): Promise<void> {
+  let response: Response = await fetch(_url);
+  let response2: string = await response.json();
+  console.log(response2);
+  /* _response.setHeader("content-type", "text/html; charset=utf-8");
+  _response.setHeader("Access-Control-Allow-Origin", "*");
 
-  /* function handleListen(): void {
-    console.log("Listening");
-  }
+  _response.write(_url);
 
-  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-    console.log("I hear voices!");
-
-    _response.setHeader("content-type", "text/html; charset=utf-8");
-    _response.setHeader("Access-Control-Allow-Origin", "*");
-
-    _response.write(_request.url);
-
-    _response.end();
-  } */
+  _response.end();  */
+  } 
 }
