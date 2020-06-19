f075301402e6b012ff777f773137f3d33d4a8fc0
corinna.sailer <corinna.jasmin.sailer@hs-furtwangen.de>
2020-06-19 10:33:36 +0200

    server

--- Aufgabe_8/client.js
+++ Aufgabe_8/client.js
@@ -3,21 +3,18 @@ var Aufgabe_8;
 (function (Aufgabe_8) {
     let button = document.getElementById("button");
     button.addEventListener("click", buttonclick);
-    async function urlAendern() {
+    async function buttonclick() {
         let formData = new FormData(document.forms[0]);
         let url = "https://dedflake.herokuapp.com/";
         let query = new URLSearchParams(formData);
         url += url + "?" + query.toString();
-        await fetch(url);
-        return url;
-    }
-    async function buttonclick() {
-        get(await urlAendern());
-    }
-    async function get(_url) {
-        let response = await fetch(_url);
-        let response2 = await response.text();
-        console.log("Response", response2);
+        let response = await fetch(url);
+        let response2 = await response.url;
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
     }
 })(Aufgabe_8 || (Aufgabe_8 = {}));
 //# sourceMappingURL=client.js.map
--- Aufgabe_8/client.ts
+++ Aufgabe_8/client.ts
@@ -2,21 +2,19 @@ namespace Aufgabe_8{
     let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
     button.addEventListener("click", buttonclick); 
 
-    async function urlAendern(): Promise<string>{
+    async function buttonclick(): Promise<void>{
         let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://dedflake.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
-        await fetch(url);
-        return url;
+        
+        let response: Response = await fetch(url);
+        let response2: string = await response.url;
+       
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
     }
-    async function buttonclick():  Promise<void> {
-      get(await urlAendern());
-    } 
-    async function get(_url: RequestInfo): Promise<void> {
-        let response: Response = await fetch(_url);
-        let response2: String = await response.text();
-        console.log("Response", response2);
-    }
-    
 }
